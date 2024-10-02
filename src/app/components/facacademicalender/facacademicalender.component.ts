bc14541cc06742a8143d4ffb5a8588bac5e07a4b
Zaki2409 <95768544+Zaki2409@users.noreply.github.com>
2024-10-02 13:49:30 +0530

    faculty academic calender component

--- src/app/components/facacademicalender/facacademicalender.component.ts
+++ src/app/components/facacademicalender/facacademicalender.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AcademicCalenderService } from 'src/app/Servcie/Academic_Calender/academic-calender.service';
+import { AcademicModel } from 'src/Model/AcademicCalender.Model';
 
 @Component({
   selector: 'app-facacademicalender',
   templateUrl: './facacademicalender.component.html',
   styleUrls: ['./facacademicalender.component.css']
 })
-export class FacacademicalenderComponent {
+export class FacacademicalenderComponent implements OnInit{
 
+  academic :AcademicModel[] = [];
+  
+  constructor (private academiccalenderService :AcademicCalenderService) {}
+  
+  
+    ngOnInit(): void {
+    
+  
+      this.academiccalenderService.getAllacademiccalender()
+      .subscribe({
+        next: (academica) => {
+           console.log(academica);
+            //to print the data from array in inspection tab 
+            this.academic = academica;
+            
+        },
+        error: (response) => {
+          console.log("cech");
+            console.log(response);
+           
+        }
+        // error: (error) => {
+        //   console.error('Error fetching employees:', error);
+        // },
+  
+  
+      })
+    }
 }
