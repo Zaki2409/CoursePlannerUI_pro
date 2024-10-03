8062301b0b3453435598c566f6214755be85dddd
Zaki2409 <95768544+Zaki2409@users.noreply.github.com>
2024-10-03 11:23:00 +0530

    faculty course component done

--- src/app/components/faccourse/faccourse.component.ts
+++ src/app/components/faccourse/faccourse.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CourseService } from 'src/app/Servcie/Course/course.service';
+import { Course } from 'src/Model/Course.Model';
 
 @Component({
   selector: 'app-faccourse',
   templateUrl: './faccourse.component.html',
   styleUrls: ['./faccourse.component.css']
 })
-export class FaccourseComponent {
+export class FaccourseComponent implements OnInit{
 
-}
+  course :Course[] = [];
+  
+  constructor (private courservice :CourseService) {}
+  
+  
+    ngOnInit(): void {
+    
+  
+      this.courservice.getAllCourse()
+      .subscribe({
+        next: (course) => {
+           console.log(course);
+            //to print the data from array in inspection tab 
+            this.course = course;
+            
+        },
+        error: (response) => {
+            console.log(response);
+           
+        }
+        // error: (error) => {
+        //   console.error('Error fetching employees:', error);
+        // },
+  
+  
+      })
+    }
+  
+  }
+  
