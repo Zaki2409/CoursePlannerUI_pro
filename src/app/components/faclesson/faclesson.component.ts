8dbf0f7a9fc4805a7f18056b1fe646c891a96de7
Zaki2409 <95768544+Zaki2409@users.noreply.github.com>
2024-10-03 10:35:56 +0530

    faculty lesson component done

--- src/app/components/faclesson/faclesson.component.ts
+++ src/app/components/faclesson/faclesson.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LessonService } from 'src/app/Servcie/Lesson/lesson.service';
+import { Lesson } from 'src/Model/Lesson.Model';
 
 @Component({
   selector: 'app-faclesson',
   templateUrl: './faclesson.component.html',
   styleUrls: ['./faclesson.component.css']
 })
-export class FaclessonComponent {
+export class FaclessonComponent implements OnInit{
 
-}
+  lesson :Lesson[] = [];
+  
+  constructor (private lessonservice :LessonService) {}
+  
+  
+    ngOnInit(): void {
+    
+  
+      this.lessonservice.getAllLesson()
+      .subscribe({
+        next: (lesson) => {
+           console.log(lesson);
+            //to print the data from array in inspection tab 
+            this.lesson = lesson;
+            
+        },
+        error: (response) => {
+            console.log(response);
+           
+        }
+        // error: (error) => {
+        //   console.error('Error fetching employees:', error);
+        // },
+  
+  
+      })
+    }
+  
+  }
+  
