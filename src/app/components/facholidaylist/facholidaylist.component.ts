15c980faf94076f71ae874b172d16c211cec99aa
Zaki2409 <95768544+Zaki2409@users.noreply.github.com>
2024-10-03 11:24:56 +0530

    faculty holiday list component done

--- src/app/components/facholidaylist/facholidaylist.component.ts
+++ src/app/components/facholidaylist/facholidaylist.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HolidaylistService } from 'src/app/Servcie/Holiday_List/holidaylist.service';
+import { HolidayList } from 'src/Model/HolidayList.Model';
 
 @Component({
   selector: 'app-facholidaylist',
   templateUrl: './facholidaylist.component.html',
   styleUrls: ['./facholidaylist.component.css']
 })
-export class FacholidaylistComponent {
+export class FacholidaylistComponent implements OnInit{
 
-}
+  holidaylist :HolidayList[] = [];
+  
+  constructor (private holidaylistService :HolidaylistService) {}
+  
+  
+    ngOnInit(): void {
+    
+  
+      this.holidaylistService.getAllhl()
+      .subscribe({
+        next: (data) => {
+           console.log(data);
+            //to print the data from array in inspection tab 
+            this.holidaylist = data;
+            
+        },
+        error: (response) => {
+            console.log(response);
+           
+        }
+        // error: (error) => {
+        //   console.error('Error fetching employees:', error);
+        // },
+  
+  
+      })
+    }
+  
+  }
+  
